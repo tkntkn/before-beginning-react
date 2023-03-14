a805f3a4987a3693427d3593d97625168cf01b1a
tkntkn <kd1049@gmail.com>
2023-03-14 22:56:38 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,8 +4,8 @@ const server = http.createServer((request, response) => {
   response.setHeader('Content-Type', 'text/html; charset=UTF-8');
   
   response.write(`
-    <body></body>
     <script>
+      console.log(document.childNodes);
       document.querySelector("body").textContent = "こんにちは！"
     </script>
   `);
