f494b3883018776501382f62d0e8fbb410ac438c
tkntkn <kd1049@gmail.com>
2023-03-14 22:49:57 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,13 +3,11 @@ const http = require('node:http');
 const server = http.createServer((request, response) => {
   response.setHeader('Content-Type', 'text/html; charset=UTF-8');
   
-  if (request.url === "/javascript") {
-    response.write(`ジャバスクリプト`);
-  } else if (request.url === "/server") {
-    response.write(`サーバー`);
-  } else {
-    response.write(`分かりません。`)
-  }
+  response.write(`
+    <script>
+    
+    </script>
+  `);
 
   response.end();
 })
