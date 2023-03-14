e19f178c788e5bda3ffff42d982884825ee5e16a
tkntkn <kd1049@gmail.com>
2023-03-14 20:28:25 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
-const http = require("node:http");
+const http = require('node:http');
 
 const server = http.createServer((request, response) => {
   response.setHeader('Content-Type', 'text/html; charset=UTF-8');
-  response.write('こんにちは。').end();
-});
+  response.write(`こんにちは。`).end();
+})
 
 server.listen('80', 'localhost');
