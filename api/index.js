b52e35eff0acfb7d5bf28a62dd2964f74fdba8c3
tkntkn <kd1049@gmail.com>
2023-03-19 06:30:45 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,20 +1,8 @@
 const http = require('node:http');
-const messages = [];
 
 const server = http.createServer((request, response) => {
-  messages.push(request.url);
-  
   response.setHeader('Content-Type', 'text/plain; charset=UTF-8');
-  
-  let text = "";
-  for (const message of messages) {
-    if (message !== "/favicon.ico") {
-      text += decodeURIComponent(message).slice(1);
-      text += "\n";
-    }
-  }
-  
-  response.write(text);
+  response.write('こんにちは。');
   response.end();
 })
 
